68882b44e2e6386f87ef31d9a3d6cde9e84c912f
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2022-10-11 00:43:09 +0300

    aaaa

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Box } from './Box/Box';
+import { Profile } from './Profile/Profile';
+import userDate from '../user.json';
+
+// const imageUrl = user.avatar;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box backgroundColor="#e5e5e5" paddingBottom="30px" paddingTop="30px">
+      <Box
+        display="flex"
+        flexDirection="column"
+        marginLeft="auto"
+        marginRight="auto"
+        alignItems="center"
+        width="300px"
+        boxShadow="0px 1px 3px rgb(0 0 0 / 12%), 0px 1px 1px rgb(0 0 0 / 14%), 0px 2px 1px rgb(0 0 0 / 20%)"
+        borderRadius="0px 0px 4px 4px"
+        backgroundColor="#ffff"
+        marginBottom="80px"
+      >
+        <Profile data={userDate}></Profile>
+      </Box>
+    </Box>
   );
 };
